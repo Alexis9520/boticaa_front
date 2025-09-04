ad1b4ecd728874e6253720caa47ac27337fb9bd8
Alexis Romani <alexisdasiel@gmail.com>
2025-09-04 01:04:10 -0500

    nueva ip4

--- components/config.ts
+++ components/config.ts
@@ -1,6 +1,6 @@
 // Archivo para centralizar la base URL y el helper apiUrl
-export const BASE_URL = "https://109.199.106.139:8443";
-// export const BASE_URL = "http://62.169.28.77:8080";
+export const BASE_URL = "https://boticasaid.quantify.net.pe";
+
 export function apiUrl(path: string) {
   if (path.startsWith("http")) return path;
   return `${BASE_URL}${path.startsWith("/") ? "" : "/"}${path}`;
